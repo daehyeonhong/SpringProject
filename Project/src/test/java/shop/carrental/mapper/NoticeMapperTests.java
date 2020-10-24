2882459888f5087f4f84efef1fcd4c1e38e2ccd8
hongdae <fholng@outlook.com>
2020-10-24 23:48:51 +0900

    test

--- Project/src/test/java/shop/carrental/mapper/NoticeMapperTests.java
+++ Project/src/test/java/shop/carrental/mapper/NoticeMapperTests.java
@@ -1,8 +1,5 @@
 package shop.carrental.mapper;
 
-import java.util.List;
-import java.util.function.Consumer;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +7,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import lombok.Setter;
-import lombok.extern.log4j.Log4j;
 import shop.carrental.domain.Criteria;
 import shop.carrental.mappers.NoticeMapper;
 
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j
 public class NoticeMapperTests {
 
 	@Setter(onMethod_ = @Autowired)
